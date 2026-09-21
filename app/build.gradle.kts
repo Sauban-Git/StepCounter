85f6f88022195579c38bda0701f737ecb77c1e8e
sauban-git <128286356+Sauban-Git@users.noreply.github.com>
2026-09-21 18:12:18 +0530

    Removed unused library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
-    implementation("com.google.android.gms:play-services-location:21.4.0")
     implementation("androidx.compose.material:material-icons-extended")
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -85,5 +84,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    ksp("androidx.room3:room3-compiler:3.0.0")
+    ksp("androidx.room3:room3-compiler:3.0.3")
 }
